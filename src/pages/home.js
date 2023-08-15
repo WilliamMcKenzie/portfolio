c000d6065c45f8cb37483395b0e3b8690d95daf9
William McKenzie <williamqmckenzie@gmail.com>
2023-08-15 13:14:06 -0300

    s

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,11 @@ import HomeComponent from "../components/home";
 import Project from "../components/project";
 import About from "../components/about";
 import '../style.css';
+import BackgroundParticle from "../components/backgroundParticle";
 
 const Home = () => {
     return (<div className="homePage">
+        <BackgroundParticle className="BackgroundParticle" />
         <HomeComponent />
         <About />
         <Project />
