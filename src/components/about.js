b546d02f691be6f47e15934ec91402cb90b2eb46
William McKenzie <williamqmckenzie@gmail.com>
2023-08-16 19:36:44 -0300

    sxx

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,7 @@ const About = () => {
         <SplashArt className="splashArt" />
         <div className="aboutContent">
             <p className="aboutHeader">About Me</p>
-            <p className="aboutP">Hey there! I'm William, a lifelong explorer from the beautiful land of Canada. With an unwavering passion for technology and a curious mind, I've always been on the lookout for new opportunities to learn and grow. My journey began at a young age when I discovered the world of pixel art, igniting my artistic spirit and leading me to become a skilled pixel artist since the age of 13. Beyond my creative endeavors, you'll find me on the Taekwondo mat, where I've earned a blue belt. I'm all about embracing challenges and pushing boundaries in both the digital and physical realms. Welcome to my corner of the web, where I blend my tech enthusiasm, artistic flair, and a constant thirst for knowledge. Join me on this exciting journey!</p>
+            <p className="aboutP">Hey there! I'm William, a aspring developer from Canada. With a passion for technology, I've always been on the lookout for new opportunities to learn and grow. Welcome to my corner of the web, where I blend my tech enthusiasm, artistic flair, and a constant thirst for knowledge. Join me on this exciting journey!</p>
             <div className="aboutFlex">
                 <div className="aboutLanguages">
                     <header>4</header>
