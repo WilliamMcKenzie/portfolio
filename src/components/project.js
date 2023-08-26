ef1d1cceec2492f3e132f05cd03a5c569277b835
William McKenzie <williamqmckenzie@gmail.com>
2023-08-26 18:24:21 -0300

    dsa

--- src/components/project.js
+++ src/components/project.js
@@ -1,4 +1,5 @@
 import teamTetherPreview from '../icons/teamtetherPreview.png'
+import linearPreview from '../icons/linearPreview.png'
 import open from '../icons/open.png'
 import { useNavigate } from "react-router-dom";
 import { motion, useScroll } from "framer-motion"
@@ -36,18 +37,18 @@ const Project = () => {
                 </div>
             </div>
         </motion.div>
-        <motion.div className=" project" initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1, x: 0, }} transition={{ duration: 0.5 }}>
-            <div className="previewContainer" onClick={() => window.open("http://williamqm.com", "_blank")}>
-                <img className="projectPreview" src="https://picsum.photos/1900/961" />
+        <motion.div className="teamTether project" initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1, x: 0, }} transition={{ duration: 0.5 }}>
+            <div className="previewContainer" onClick={() => window.open("http://linear.williamqm.com", "_blank")}>
+                <img className="projectPreview" src={linearPreview} />
                 <img className="projectOpen" src={open} />
             </div>
             <div className="projectDetails">
-                <p className="projectTitle">Placeholder</p>
-                <div className="projectTechnologies">
-                    <p className="projectHeader">Technology</p>
-                </div>
+                <p className="projectTitle">TeamTether</p>
                 <div className="projectTechnologies">
                     <p className="projectHeader">Languages</p>
+                    <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"></img>
+                    <img src="https://img.shields.io/badge/HTML-D46719?style=for-the-badge&logo=html5&logoColor=white"></img>
+                    <img src="https://img.shields.io/badge/CSS-317791?&style=for-the-badge&logo=css3&logoColor=white"></img>
                 </div>
             </div>
         </motion.div>
