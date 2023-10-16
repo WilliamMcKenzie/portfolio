0cde0e1f50b09ec3bfe039f354eae1a5483a9a0f
William McKenzie <williamqmckenzie@gmail.com>
2023-10-16 19:32:37 -0300

    russ

--- src/components/project.js
+++ src/components/project.js
@@ -1,6 +1,7 @@
 import teamTetherPreview from '../icons/teamtetherPreview.png'
 import linearPreview from '../icons/linearPreview.jpg'
 import pixelPreview from '../icons/pixelPreview.png'
+import bulkinatorPreview from '../icons/bulkinatorPreview.jpg'
 import open from '../icons/open.png'
 import { useNavigate } from "react-router-dom";
 import { motion, useScroll } from "framer-motion"
@@ -15,6 +16,31 @@ const Project = () => {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
         />
+        <motion.div className="teamTether project" initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1, x: 0, }} transition={{ duration: 0.5 }}>
+            <div className="previewContainer" onClick={() => window.open("http://linear.williamqm.com", "_blank")}>
+                <img className="projectPreview" src={bulkinatorPreview} />
+                <img className="projectOpen" src={open} />
+            </div>
+            <div className="projectDetails">
+                <p className="projectTitle">Bulkinator</p>
+                <div className="projectTechnologies">
+                    <p className="projectHeader">Technology</p>
+                    <img src="https://img.shields.io/badge/NextJS-white?style=for-the-badge&logo=next.js&logoColor=black"></img>
+                    <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB"></img>
+                    <img src="https://img.shields.io/badge/Prisma-07405E?style=for-the-badge&logo=prisma&logoColor=FFFFFF"></img>
+                    <img src="https://img.shields.io/badge/MongoDB-00684A?style=for-the-badge&logo=mongodb&logoColor=FFFFFF"></img>
+                    <img src="https://img.shields.io/badge/Axios-671DDF?style=for-the-badge&logo=axios&logoColor=FFFFFF"></img>
+                </div>
+                <div className="projectTechnologies">
+
+                    <p className="projectHeader">Languages</p>
+                    <img src="https://img.shields.io/badge/Typescript-3178C6?style=for-the-badge&logo=typescript&logoColor=white"></img>
+                    <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"></img>
+                    <img src="https://img.shields.io/badge/HTML-D46719?style=for-the-badge&logo=html5&logoColor=white"></img>
+                    <img src="https://img.shields.io/badge/CSS-317791?&style=for-the-badge&logo=css3&logoColor=white"></img>
+                </div>
+            </div>
+        </motion.div>
         <motion.div className="teamTether project" initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1, x: 0, }} transition={{ duration: 0.5 }}>
             <div className="previewContainer" onClick={() => window.open("http://teamtether.williamqm.com/register", "_blank")}>
                 <img className="projectPreview" src={teamTetherPreview} />
